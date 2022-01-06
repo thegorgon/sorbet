e8c0307098b1b3e4d77b845bc17b896dd931530c
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-05 17:32:11 -0800

    Change the errors for an edge case test
    We still produce the same parse tree, but we don't arrive at the same
    part of the parse result that would have emitted an error about a module
    definition in a method body.
    
    But also, that error doesn't really make sense here? This module is
    clearly not inside a method body.
    
    So I think we're fine to just update the test and move on.

--- test/testdata/parser/offset0.rb
+++ test/testdata/parser/offset0.rb
@@ -1,4 +1,7 @@
 # typed: true
-module Model # error: module definition in method body
-  def a-b; end # error: unexpected token
+module Model
+  def a-b; end
+  #    ^ error: unexpected token "-"
+  #     ^ error: Method `b` does not exist on `T.class_of(<root>)`
+  #        ^^^ error: unexpected token "end"
 end
